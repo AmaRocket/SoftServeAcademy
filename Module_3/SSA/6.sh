423f4cb0dd20fc6de0bec8a1b543c15126e8aa29
AmaRocket <alexeverdie999@gmail.com>
2021-11-28 23:40:46 +0200

    add module_4 1st task

--- Module_3/SSA/6.sh
+++ Module_3/SSA/6.sh
@@ -2,7 +2,7 @@
 
 # What search bots have accessed the site? (UA + IP)
 
-awk '{print $15}' ~/SSA/apache_logs.txt | grep bot | uniq -c | sort -gr | tr -d + | head -n 5| sed -e 's/^.\{,3\}//;s/.\{,3\}$//'
+awk '{print $15}' ~/SSA/apache_logs.txt | grep compatible | uniq -c | sort -gr | tr -d + | head -n 100| sed -e 's/^.\{,3\}//;s/.\{,3\}$//'
 
 
 # grep bot - seach pattern in 15th colmn
